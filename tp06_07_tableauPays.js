a84d3ac72df0f22c3e94ea84a498d39d94e34441
Kevin Le Heurt <leheurt.kevin@gmail.com>
2022-01-20 15:09:19 +0100

    TP MIN & MAX

--- tp06_07_tableauPays.js
+++ tp06_07_tableauPays.js
@@ -1,15 +1,25 @@
-function main(){
+function main(min, max){
     let data = fetch('https://digicode.cleverapps.io/json/pays/pollution')
         .then(res => res.text())
         .then(JSON.parse)
+        .then(x => minMax(x,min,max))
         .then(traitement);
 
 }
+
+function minMax(data,min,max){
+    data.pays = data.pays.filter(x => x.valeur >= min && x.valeur <= max);
+    return data;
+}
+
 
 function traitement(data){
     console.log(data);
     let titre = document.querySelector("#titre");
     let contenu = document.querySelector("#pays"); 
+
+    titre.innerHTML = "";
+    contenu.innerHTML = "";
     
     titre.innerHTML += "Pays les plus polluants pour le "+data.polluant+"("+data.unite+") en "+data.annee; 
     data = data.pays;
@@ -22,4 +32,30 @@ function traitement(data){
             "<td>"+data[i].pourcentage+"</td>"+
             "</tr>";
     }
+}
+
+function alerte1(){
+    let msg = document.querySelector("#msg");
+    msg.innerHTML += "<div class='alert alert-danger' role='alert'>Min ne peut être supérieur ou égal à Max</div>"
+}
+
+function alerte2(){
+    let msg = document.querySelector("#msg");
+    msg.innerHTML += "<div class='alert alert-danger' role='alert'>Saisir des nombres positifs</div>"
+}
+
+function filtrer(){
+    let msg = document.querySelector("#msg");
+    msg.innerHTML = "";
+
+    let min = document.querySelector("#min").value;
+    let max = document.querySelector("#max").value;
+
+    if(min == "") min = 0;
+    if(max == "") max = Number.MAX_VALUE;
+
+    if(min >= max) alerte1();
+    if(min < 0 || max < 0)  alerte2();
+    
+    main(min,max);
 }
